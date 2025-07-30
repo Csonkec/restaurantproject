f69c019d4907443f8d11779571b766892f64f8cc
Csonka Marton <csonkamarci3@gmail.com>
2025-07-30 09:53:48 +0200

    navbar

--- nav.php
+++ nav.php
@@ -9,5 +9,31 @@
 </head>
 <body>
     
+        <nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark" style="--bs-bg-opacity: .85; backdrop-filter: blur(10px) !important; -webkit-backdrop-filter: blur(10px)!important;">
+            <div class="container">
+                <a href="index.php" class="navbar-brand mb-0"><img class="d-inline-block mb-1 me-1" src="assets/icon.png" width="30" height="25"/>Adrenalin BBQ & GRILL</a>
+            
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a href="#" class="nav-link">Asztalfoglalás</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Menü</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Kapcsolat</a></li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profil</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#" class="dropdown-item">Bejelentkezés</a></li>
+                                <li><a href="#" class="dropdown-item">Regisztrálás</a></li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    
 </body>
 </html>
