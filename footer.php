14b4cfeb53f88bb880700304404d6519693d49ab
Csonka Marton <csonkamarci3@gmail.com>
2025-08-01 11:11:24 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,77 @@
+<head>
+    <style>
+        @media (max-width: 1024px){
+            .footer-flex{
+                flex-direction: column;
+            }
+        }
+        @media (max-width: 768px){
+            .footer-iframe{
+                width: 300px;
+                height: 200px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <footer class="container-fluid bg-dark text-light pb-4 pt-4">
+        <div class="container">
+            <div class="d-flex justify-content-around align-items-center footer-flex">
+                <div>
+                    <h2 class="text-center">Nyitvatartás: </h2>
+                    <ul class="list-group-flush">
+                        <li class="list-group-item">
+                            Hétfő: Nem dolgozunk
+                        </li>
+                        <li class="list-group-item">
+                            Kedd: 12:00 - 22:00
+                        </li>
+                        <li class="list-group-item">
+                            Szerda: 12:00 - 22:00
+                        </li>
+                        <li class="list-group-item">
+                            Csütörtök: 12:00 - 22:00
+                        </li>
+                        <li class="list-group-item">
+                            Péntek: 12:00 - 23:00
+                        </li>
+                        <li class="list-group-item">
+                            Szombat: 12:00 - 23:00
+                        </li>
+                        <li class="list-group-item">
+                            Vasárnap: Nem dolgozunk
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="text-center">Elérhetőségek:</h2>
+                    <ul class="list-group-flush">
+                        <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                            </svg> +381631227487
+                        </li>
+                        <li class="list-group-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+                            </svg> Molski put 3, ADA
+                        </li>
+                        <li class="list-group-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                            </svg> jezeroadrenalinada@gmail.com
+                        </li>
+                        <li class="list-group-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-at" viewBox="0 0 16 16">
+                            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914"/>
+                            </svg> jezeroadrenalinada
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <iframe class="footer-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.1581611096794!2d20.13801232314847!3d45.788059031699404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4744c9000e20dcad%3A0x3a74298fae8536ca!2sAdrenalin%20BBQ%20%26%20GRILL!5e0!3m2!1shu!2srs!4v1754037734429!5m2!1shu!2srs" width="450" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    </footer>
+</body>
