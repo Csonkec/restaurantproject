88dabb02b9791613c9c8b39275a73c76043f98a3
Csonka Marton <csonkamarci3@gmail.com>
2025-08-01 08:47:52 +0200

    hero section

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
 <html lang="en">
 <head>
     <title>Adrenalin BBQ & GRILL</title>
+    <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
-    
+    <section class="hero">
+        <p class="fw-bold text-light text-shadow">Foglalj asztalt könnyedén nálunk!</p>
+        <a href="#" class="btn btn-danger btn-lg">Foglalás most</a>
+    </section>
 </body>
 </html>
