fcc8daf0020c8ecf7c78686eaa41b9a08eabcc4e
Csonka Marton <csonkamarci3@gmail.com>
2025-08-13 12:31:28 +0200

    heto section

---
+++ reservation.php
@@ -0,0 +1,16 @@
+<?php 
+    include "nav.php";
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>Adrenalin BBQ & GRILL</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <section class="hero mb-5">
+        <p class="fw-bold text-light text-shadow">Asztalfoglalás</p>
+        <p class="text-light text-shadow">Válaszd ki a dátumot, az időpontot és az asztalt, majd erősítsd meg a foglalásodat.</p>
+    </section>
+</body>
